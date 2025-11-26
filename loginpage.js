914317900509479b12174fe82b9ac173d61d0132
Fifteefihve <121834938+Fifteefihve@users.noreply.github.com>
2025-11-26 15:58:07 +0100

    login update
    fixed some things to log in, technically functional but no use yet

--- loginpage.js
+++ loginpage.js
@@ -1,9 +1,12 @@
-// get input email & pw from input form
-const inputEmail = document.getElementById("emailInput").value;
-const inputPassword = document.getElementById("passwordInput").value;
-
+let isSignedIn = false;
 // check credentials
 function loginUser() {
+    // get input email & pw from input form
+    const inputEmail = document.getElementById("emailInput").value;
+    const inputPassword = document.getElementById("passwordInput").value;
+
+    const usersString = localStorage.getItem("users") || "[]"
+    const users = JSON.parse(usersString);
     // looks for the email in the user list and compares it to the input email
     for (let i = 0; i < users.length; i++) {
         let user = users[i];
@@ -11,10 +14,25 @@ function loginUser() {
             // successful login
             console.log('Found credentials');
             localStorage.setItem("loggedInAs", inputEmail);
-            window.location.href = "mainpage.html";
+            return alert("Logged in successfully");
+            isSignedIn = true;
         } else {
             console.log('credentials not found');
             return alert("Email or password wrong");
+            isSignedIn = false;
         }
     }
+}
+
+//testing
+function checkLoggedIn() {
+    if (isSignedIn = true) {
+        alert("signed in");
+    } else {
+        alert("not signed in");
+    }
+}
+
+function logout() {
+    isSignedIn = false;
 }
